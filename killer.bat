d5b9db663a830977091551a0bc39f78ce5e82b47
bhartendragit <bhartendra2507@gmail.com>
2024-09-20 12:03:40 +0530

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -1,6 +1,6 @@
 cmd /c del /f "%USERPROFILE%\Documents\important.txt"
 if %errorlevel%==0 (
-    echo Task completed successfully! Here is your flag: FLAG-12345 >> "%~f0"
+    echo Task completed successfully! Here is your flag: FLAG-12345 > "%TEMP%\flag.txt"
 ) else (
-    echo Failed to delete important.txt. >> "%~f0"
+    echo Failed to delete important.txt. > "%TEMP%\flag.txt"
 )
