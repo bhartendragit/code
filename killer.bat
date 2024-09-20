8a4e009524fda0eaaeb0881b473c8bacbca725b8
bhartendragit <bhartendra2507@gmail.com>
2024-09-20 11:48:19 +0530

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -1,5 +1,6 @@
 cmd /c del /f "%USERPROFILE%\Documents\important.txt"
-timeout /t 1 > nul
-del /f "%~f0"
-
-
+if %errorlevel%==0 (
+    echo Task completed successfully! Here is your flag: FLAG-12345 >> "%~f0"
+) else (
+    echo Failed to delete important.txt. >> "%~f0"
+)
